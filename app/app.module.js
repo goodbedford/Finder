e6bbd157108ff87b435824aa40bcb6eaa9f0155d
goodbedford <bedford@goodbedford.com>
2016-01-28 14:09:13 -0800

    added module depends to app.module

---
+++ app/app.module.js
@@ -0,0 +1,20 @@
+(function(){
+    "use strict";
+
+
+    angular
+        .module("finderApp", [
+
+            //angular core modules
+            "ngAnimate",
+            "ngAria",
+            "ngMaterial",
+            "ngSanitize",
+            "ngMessages",
+            "ngResource",
+            //third party
+            "ui.router"
+        ])
+
+
+})();
